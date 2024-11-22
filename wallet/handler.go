976882156de70c81ff0623cd21239c39ac98b14b
panutrytobeprogrammer <s.panuwatb@gmail.com>
2024-11-22 23:40:55 +0700

    add summary per month

--- wallet/handler.go
+++ wallet/handler.go
@@ -2,6 +2,7 @@ package wallet
 
 import (
 	"database/sql"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
@@ -48,8 +49,21 @@ func (r *walletHandler) NewTransaction(c *gin.Context) {
 func (r *walletHandler) GetSummary(c *gin.Context) {
 	usr := c.MustGet("user").(*UserModel)
 
-	sql := `select sum(amount), transaction_type from transactions where user_id = $1 group by transaction_type`
-	rows, err := r.db.Query(sql, usr.UserID)
+	now := time.Now()
+	var startDate time.Time
+	var endDate time.Time
+	if now.Day() >= 23 {
+		startDate = time.Date(now.Year(), now.Month(), 23, 0, 0, 0, 0, time.UTC)
+		nextMonth := now.AddDate(0, 1, 0)
+		endDate = time.Date(nextMonth.Year(), nextMonth.Month(), 22, 23, 59, 59, 999999999, time.UTC)
+	} else {
+		lastMonth := now.AddDate(0, -1, 0)
+		startDate = time.Date(lastMonth.Year(), lastMonth.Month(), 23, 0, 0, 0, 0, time.UTC)
+		endDate = time.Date(now.Year(), now.Month(), 22, 23, 59, 59, 999999999, time.UTC)
+	}
+
+	sql := `select sum(amount), transaction_type from transactions where user_id = $1 and transaction_date BETWEEN $2 AND $3 group by transaction_type`
+	rows, err := r.db.Query(sql, usr.UserID, startDate, endDate)
 	if err != nil {
 		r.logger.Error("failed to get summary", zap.String("error", err.Error()))
 		c.JSON(500, gin.H{"error": "failed to get summary"})
@@ -69,11 +83,11 @@ func (r *walletHandler) GetSummary(c *gin.Context) {
 		if transactionType == "Income" {
 			income += amount
 		} else {
-			expense -= amount
+			expense += amount
 		}
 	}
 
-	c.JSON(200, gin.H{"income": income / 1000, "expense": expense / 1000})
+	c.JSON(200, gin.H{"income": income / 1000, "expense": expense / 1000, "balance": income/1000 - expense/1000})
 	return
 }
 
@@ -97,4 +111,8 @@ func (r *walletHandler) GetSummary(c *gin.Context) {
 
 // 	c.JSON(200, gin.H{"message": "success"})
 // 	return
+// }
+
+// func (r *walletHandler) Reset()  {
+
 // }
